cbdfb808a64aa4bb51efc7b22af18b92ba82b97a
alopezW <alopez@wantudu.com>
2014-02-20 20:41:38 +0000

    Initial Commit

---
+++ iostestsimpleexample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iostestsimpleexample
+//
+//  Created by aaron lopez on 20/02/14.
+//  Copyright (c) 2014 aaron lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iostestsimpleexampleTests/iostestsimpleexampleTests.m
@@ -0,0 +1,34 @@
+//
+//  iostestsimpleexampleTests.m
+//  iostestsimpleexampleTests
+//
+//  Created by aaron lopez on 20/02/14.
+//  Copyright (c) 2014 aaron lopez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iostestsimpleexampleTests : XCTestCase
+
+@end
+
+@implementation iostestsimpleexampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
